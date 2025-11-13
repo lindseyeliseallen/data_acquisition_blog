a31458b7f60c47e29936a74750643f2181ebdaf7
Lindsey <lindseyeliseallen@gmail.com>
2025-11-12 19:29:29 -0700

    setup API key access

--- trending_vids.py
+++ trending_vids.py
@@ -4,3 +4,11 @@ import pandas as pd
 import time
 import os
 
+
+load_dotenv()
+
+API_KEY = os.getenv("YOUTUBE_API_KEY") 
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=API_KEY)
